efaf3defef5761276ec009764dd7db06565b5f19
Iryna Yelunina <lucky.asterisk@gmail.com>
2023-08-15 22:41:30 +0200

    true code

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,64 @@
 //в классе SmartArray реализовать удаление элемента из массива
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
+    static Random random = new Random();
+
     public static void main(String[] args) {
-        SmartArray array = new SmartArray();
+        SmartArray smartArray = new SmartArray();
 
-        System.out.println(array);
+        generateValueForArray(smartArray, 17);
 
-        array.add(1);
-        System.out.println(array);
-        array.add(77);
-        System.out.println(array);
-        array.add(51);
-        System.out.println(array);
-        array.add(1);
-        System.out.println(array);
-        array.add(9);
-        System.out.println(array);
-        array.add(7);
-        System.out.println(array);
-        array.add(11);
-        System.out.println(array);
-        array.add(23);
-        System.out.println(array);
-        array.add(65);
-        System.out.println(array);
-        array.add(1);
-        System.out.println(array);
-        array.add(77);
-        System.out.println(array);
-        array.add(51);
-        System.out.println(array);
-        array.add(1);
-        System.out.println(array);
-        array.add(77);
-        System.out.println(array);
-        array.add(22);
-        System.out.println(array);
+        smartArray.changeValue(5, 99);
 
-        int[] qwe = array.getArray();
-        qwe[0] = 555555;
-        System.out.println(array);
+        System.out.println(smartArray.toString());
+        System.out.println(smartArray.toFullString());
 
+        smartArray.deleteByValue(99);
+        smartArray.deleteByIndex(-10);
+        System.out.println(smartArray.toString());
+        System.out.println(smartArray.toFullString());
+
+        System.out.println();
+        System.out.println("==================");
+        smartArray.trim();
+
+        smartArray.changeValue(3, 99);
+        smartArray.deleteByValue(99);
+
+
+        System.out.println(smartArray.toString());
+        System.out.println(smartArray.toFullString());
+
+        smartArray.deleteByChicago(2);
+
+
+        int[] ints = {25, 13, 54, 55, 4};
+
+        SmartArray sArray = new SmartArray(ints);
+        System.out.println(sArray.toString());
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+    static void generateValueForArray(SmartArray smartArray, int count) {
+        for (int i = 0; i < count; i++) {
+            smartArray.add(random.nextInt(101));
+        }
     }
 }
--- src/SmartArray.java
+++ src/SmartArray.java
@@ -2,54 +2,120 @@ import java.util.Arrays;
 
 public class SmartArray {
     private int size = 0;
-    private int[] array = new int[5];
+    private int[] arr;
+
+    public SmartArray() {
+        arr = new int[4];
+    }
+
+    public SmartArray(int[] ints) {
+        this();
+        add(ints);
+    }
 
     public void add(int element) {
-        if (size >= array.length) {
+        if (size == arr.length) {
             increaseArray();
         }
-        array[size] = element;
+        arr[size] = element;
         size++;
     }
 
-
-    private void increaseArray() {
-        int[] temparray = new int[array.length * 2];
-        for (int i = 0; i < array.length; i++) {
-            temparray[i] = array[i];
+    public void add(int... ints) {
+        for (int value : ints) {
+            add(value);
         }
-        array = temparray;
     }
 
-    public String spesialToString() {
-        return Arrays.toString(array) + "   size:" + size;
+    private void increaseArray() {
+        int[] tempArray = new int[arr.length * 2];
+        for (int i = 0; i < arr.length; i++) {
+            tempArray[i] = arr[i];
+        }
+        arr = tempArray;
+    }
+
+    public String specialToString() {
+        return Arrays.toString(arr) + "   size: " + size;
     }
 
     public int[] getArray() {
-        int[] temparray = new int[array.length];
-        for (int i = 0; i < array.length; i++) {
-            temparray[i] = array[i];
+        int[] tempArray = new int[size];
+        for (int i = 0; i < size; i++) {
+            tempArray[i] = arr[i];
         }
-        return temparray;
+        return tempArray;
     }
 
-    public String toString() {
+    public int searchByValue(int value) {
+
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == value) return i;
+        }
+        return -1;
+    }
+
+    public boolean deleteByIndex(int index) {
+        if (index < 0 || index > size) return false;
+        for (int i = index; i < size - 1; i++) {
+            // 6, arr[6] = arr[7];
+            arr[i] = arr[i + 1];
+        }
+        arr[size - 1] = 0;
+        size--;
+        return true;
+    }
+
+    public void deleteByChicago(int index) {
+        int[] newArray = new int[arr.length - 1];
+        System.arraycopy(arr, 0, newArray, 0, index);
+        System.arraycopy(arr, index + 1, newArray, index, newArray.length - index);
+        System.out.println(Arrays.toString(newArray));
+        arr = newArray;
+        size--;
+    }
+
+    public boolean deleteByValue(int value) {
+        int idxDeletingElement = searchByValue(value);
+        if (idxDeletingElement == -1) return false;
+
+        return deleteByIndex(idxDeletingElement);
+    }
+
+    public String toString() { //метод распечатывает только добавленные пользователем значения
         String res = "[";
         for (int i = 0; i < size; i++) {
-            res += " " + array[i];
+            res += arr[i] + (i < size - 1 ? ", " : "");
         }
         res += "]";
         return res;
     }
 
-    public void remove(int index) {
-        if (index >= 0 && index < size) {
-            for (int i = index + 1; i < size; i++) {
-                array[i - 1] = array[i];
-            }
-            size--;
+    public String toFullString() { // метод распечатывает в консоль весь массив
+        String res = "[";
+        for (int i = 0; i < arr.length; i++) {
+            res += arr[i] + (i < arr.length - 1 ? ", " : "");
         }
-
+        res += "]";
+        return res;
     }
 
+    public void changeValue(int index, int value) {
+        if (index < 0 || index > size) return;
+        arr[index] = value;
+    }
+
+    public void trim() {
+        int[] result = new int[size];
+        for (int i = 0; i < size; i++) {
+            result[i] = arr[i];
+        }
+        arr = result;
+    }
+
+
 }
+
+/*
+в классе SmartArray реализовать удаление элемента из массива
+ */
